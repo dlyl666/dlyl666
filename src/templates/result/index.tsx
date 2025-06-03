01b6b50dcb8d0bbf4cfdf0f9b6ed3e02cecfedb5
PLCNB <88578509+dlyl666@users.noreply.github.com>
2025-06-03 10:45:36 +0800

    Add src/templates/result/index.tsx

---
+++ src/templates/result/index.tsx
@@ -0,0 +1,21 @@
+import { ReadmeResult, Panel } from 'components';
+
+import { PanelsEnum } from 'types';
+import * as S from './styles';
+
+const ResultTemplate = () => {
+  return (
+    <S.Container>
+      <Panel initialPanel={PanelsEnum.GENERATED_FILES} side="left" />
+
+      <S.Wrapper>
+        <ReadmeResult />
+      </S.Wrapper>
+
+      <Panel initialPanel={PanelsEnum.RECOMMENDED_RESOURCES} side="right" />
+      <div />
+    </S.Container>
+  );
+};
+
+export { ResultTemplate };
