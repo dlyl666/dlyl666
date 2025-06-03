c8907b644180a668a05cf7e05830b5a69573ea94
PLCNB <88578509+dlyl666@users.noreply.github.com>
2025-06-03 10:42:48 +0800

    Add src/components/footer/index.tsx

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from './container';
+import { FooterOwner } from './owner';
+import { FooterNavs } from './navs';
+import { FooterGenericLink } from './generic-link';
+
+const Footer = {
+  Container,
+  Owner: FooterOwner,
+  Navs: FooterNavs,
+  GenericLink: FooterGenericLink,
+};
+
+export { Footer };
