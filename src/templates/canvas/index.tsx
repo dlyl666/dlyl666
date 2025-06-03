fa4d3145a6c9a9a2fbd256c780d8eaf759113e47
PLCNB <88578509+dlyl666@users.noreply.github.com>
2025-06-03 17:39:22 +0800

    Add src/templates/canvas/index.tsx

---
+++ src/templates/canvas/index.tsx
@@ -0,0 +1,33 @@
+import { useEffect } from 'react';
+
+import { Canvas, Footer, Panel } from 'components';
+
+import { api } from 'services';
+import { PanelsEnum } from 'types';
+
+import * as S from './styles';
+
+const CanvasTemplate = () => {
+  useEffect(() => {
+    api.put('visits');
+  }, []);
+
+  return (
+    <S.Container>
+      <Panel initialPanel={PanelsEnum.NEW_SECTION} side="left" />
+
+      <S.Wrapper>
+        <Canvas />
+        <Footer.Container>
+          <Footer.Owner />
+          <Footer.Navs />
+          <Footer.GenericLink href="/result" label="Generate README" />
+        </Footer.Container>
+      </S.Wrapper>
+
+      <Panel initialPanel={PanelsEnum.RECOMMENDED_RESOURCES} side="right" />
+    </S.Container>
+  );
+};
+
+export { CanvasTemplate };
