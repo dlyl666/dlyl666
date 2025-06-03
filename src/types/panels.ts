029fd3d72d320e4008daa0015083d16b3cfe38b6
PLCNB <88578509+dlyl666@users.noreply.github.com>
2025-06-03 17:40:00 +0800

    Add src/types/panels.ts

---
+++ src/types/panels.ts
@@ -0,0 +1,15 @@
+import { Sections as EditPanels } from '.';
+import { ValueOf } from './helpers';
+
+enum OtherPanels {
+  NEW_SECTION = 'new-section',
+  GENERATED_FILES = 'generated-files',
+  TEMPLATES = 'templates',
+  RECOMMENDED_RESOURCES = 'recommended-resources',
+  USER_SETTINGS = 'user-settings',
+}
+
+export type PanelSide = 'right' | 'left';
+
+export const PanelsEnum = { ...EditPanels, ...OtherPanels } as const;
+export type PanelsEnumType = ValueOf<typeof PanelsEnum>;
